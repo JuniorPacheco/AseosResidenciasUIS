bfd1d9e7e3cb77517f9adf91cefde7e0fd19ce98
Junior Pacheco <junior.pacheco.356@gmail.com>
2022-09-17 17:34:35 -0500

    agregada funcion de ver los aseos del dia tocando cierta fecha

---
+++ src/utils/getAseos.js
@@ -0,0 +1,81 @@
+import dayjs from "dayjs";
+
+const getDateDiff = (date, difference = 0) => {
+  const originDate = dayjs(0);
+  const newDateWithDiff = date.add(difference, "days");
+  const daysDiff = newDateWithDiff.diff(originDate, "days", true);
+  const integerDaysDiff = Math.floor(daysDiff);
+  return integerDaysDiff;
+};
+
+export const getAseoPasilloPrimerPiso = (date, difference = 0) => {
+  const cycleCleanliness = [101, 0, 102, 0, 103, 0, 104, 0, 105, 0];
+  const cuantityRooms = cycleCleanliness.length;
+
+  const integerDaysDiff = getDateDiff(date, difference);
+
+  const indexHab = integerDaysDiff % cuantityRooms;
+  const currenttHab = cycleCleanliness[indexHab];
+  return currenttHab;
+};
+
+export const getAseoPasilloSegundoPiso = (date, difference = 0) => {
+  const cycleCleanliness = [202, 0, 203, 0, 204, 0, 205, 0, 201, 0];
+  const cuantityRooms = cycleCleanliness.length;
+
+  const integerDaysDiff = getDateDiff(date, difference);
+
+  const indexHab = integerDaysDiff % cuantityRooms;
+  const currenttHab = cycleCleanliness[indexHab];
+  return currenttHab;
+};
+
+export const getAseoPasilloTercerPiso = (date, difference = 0) => {
+  const cycleCleanliness = [303, 0, 304, 0, 305, 0, 301, 0, 302, 0];
+  const cuantityRooms = cycleCleanliness.length;
+
+  const integerDaysDiff = getDateDiff(date, difference);
+
+  const indexHab = integerDaysDiff % cuantityRooms;
+  const currenttHab = cycleCleanliness[indexHab];
+  return currenttHab;
+};
+
+export const getAseoCocina = (date, difference = 0) => {
+  const cycleCleanliness = [
+    105, 0, 204, 0, 303, 0, 101, 0, 205, 0, 304, 0, 104, 0, 203, 0, 302, 0, 102,
+    0, 201, 0, 305, 0, 301, 0, 202, 0, 103, 0,
+  ];
+  const cuantityRooms = cycleCleanliness.length;
+
+  const integerDaysDiff = getDateDiff(date, difference);
+
+  const indexHab = integerDaysDiff % cuantityRooms;
+  const currenttHab = cycleCleanliness[indexHab];
+  return currenttHab;
+};
+
+export const getAseoSala = (date, difference = 0) => {
+  const cycleCleanliness = [
+    104, 0, 302, 0, 203, 0, 103, 0, 202, 0, 301, 0, 101, 0, 205, 0, 304, 0, 305,
+    0, 102, 0, 201, 0, 105, 0, 204, 0, 303, 0,
+  ];
+  const cuantityRooms = cycleCleanliness.length;
+
+  const integerDaysDiff = getDateDiff(date, difference);
+
+  const indexHab = integerDaysDiff % cuantityRooms;
+  const currenttHab = cycleCleanliness[indexHab];
+  return currenttHab;
+};
+
+export const getAllAseos = (date, difference = 0) => {
+  const AllAseos = {
+    primerPiso: getAseoPasilloPrimerPiso(date, difference),
+    segundoPiso: getAseoPasilloSegundoPiso(date, difference),
+    tercerPiso: getAseoPasilloTercerPiso(date, difference),
+    cocina: getAseoCocina(date, difference),
+    sala: getAseoSala(date, difference),
+  };
+  return AllAseos;
+};
